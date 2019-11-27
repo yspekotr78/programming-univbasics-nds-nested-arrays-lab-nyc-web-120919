707bf400d147ea33119ecc85b786d21f4db1d02f
Yury Spektor <yspektor78@gmail.com>
2019-11-27 19:44:53 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -23,20 +23,7 @@ def assembled_matrix
 end
 
 def array_literal_matrix
-   res =[[
-  "Don Gately",
-  "Joelle van Dyne",
-  "Pat M.",
-  "Kate Gompert",
-  "Bruce Green"
-],[
-  "Hal Incandenza",
-  "Lyle",
-  "Gerhard Schtitt",
-  "Mario Incandenza",
-  "Michael Pemulis"
-]]
-  new_res=res.sort
+
   return outside = [ENNET_HOUSE.sort,ENFIELD_TENNIS_ACADEMY.sort]
   # Using Array literal syntax only, build a nested array that uses the data in
   # side the ENNET_HOUSE and ENFIELD_TENNIS_ACADEMY Arrays but sorts
